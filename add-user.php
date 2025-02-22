91cf0417ad7c7ec2d2e75126c886b9712a716688
Steven Gomez <steve_gomez@usa.net>
2025-02-21 21:21:27 -0600

    Ready to start Module 7.3

---
+++ add-user.php
@@ -0,0 +1,39 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Users</title>
+</head>
+<body>
+    <section>
+        <h2>Register Form</h2>
+        <form action="add-user.php" method ="post">
+        <div class="container">
+
+            <label for="uname"><b>Username</b></label>
+            <input type="text" placeholder="Username" name="username" required>
+            <br />
+
+            <label for="psw"><b>Password</b></label>
+            <input type="password" placeholder="Enter Password" name="password" required>
+            <br />
+
+            <label for="email"><b>Email</b></label>
+            <input type="password" placeholder="Enter Email" name="email" required>
+            <br />
+
+            <button type="submit" name="submit">Signup</button>
+        </div>
+        </form>
+        <div class="container">
+            <a href="users.php" class="footerbtn">Users</a>
+
+            <a href="logout.php" class="footerbtn">Log out</a>
+        </div>
+    </section>
+    
+</body>
+</html>
+
